b7bc7d63c314e250cc4c2b2560d9333219b08d01
Matheus Boechat <matheusfgb@gmail.com>
2023-02-06 22:23:40 -0300

    acertando comentários

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-//  Função para gerar cor aleatória
+// Requisito 4
 const generateRandomColor = () => {
   const rRandom = Math.floor(Math.random() * (255 - 0) + 0);
   const gRandom = Math.floor(Math.random() * (255 - 0) + 0);
   const bRandom = Math.floor(Math.random() * (255 - 0) + 0);
   return `rgb(${rRandom}, ${gRandom}, ${bRandom})`;
 };
-//  Capturando lista das cores e botão
-const colorBoxes = document.getElementsByClassName('color');
-const button = document.getElementById('button-random-color');
-// Criando objeto para estocar cores da paleta no localStorage
 
-// Criando objeto para estocar cores no localStorage, ou repopulando se já houverem pixels estocados.
+// Requisito 5
 const createPaletteColorsObj = () => {
   if (localStorage.getItem('colorPalette') === null) {
     return {};
   }
   return JSON.parse(localStorage.getItem('colorPalette'));
 };
-
 const paletteColorsStock = createPaletteColorsObj();
 
-//  Adicionando event listener ao botão
+// Requisito 4
+const colorBoxes = document.getElementsByClassName('color');
 const changeColor = () => {
   for (let index = 1; index < colorBoxes.length; index += 1) {
-    // variável para estocar valores no localStorage
-    const boxName = colorBoxes[index].id;
     colorBoxes[index].style.backgroundColor = generateRandomColor();
     const color = colorBoxes[index].style.backgroundColor;
-    // Estocando valores no objeto
+    // Requisito 5
+    const boxName = colorBoxes[index].id;
     paletteColorsStock[boxName] = color;
-    // estocando valores no localStorage
     localStorage.setItem('colorPalette', JSON.stringify(paletteColorsStock));
   }
 };
+const button = document.getElementById('button-random-color');
 button.addEventListener('click', changeColor);
-// Recuperando obj da localStorage
+// Requisito 5
 const paletteColorsFromStorage = JSON.parse(localStorage.getItem('colorPalette'));
-//  Atribuindo palleteColorsStock, guardado no localStorage, a paleta de cores
 const localStorageColorsPalette = () => {
   for (let index = 1; index < colorBoxes.length; index += 1) {
     const boxName = colorBoxes[index].id;
@@ -49,17 +43,13 @@ if (paletteColorsFromStorage !== null) {
 //  Adicionando quadro com 25px:
 // Capturando elementos que serão usados
 const pixelBoard = document.getElementById('pixel-board');
-const resetButton = document.getElementById('clear-board');
 
-// Estocando valor para atribuir a id dos pixels;
+// Requisito 6
 let pixelNumber = 0;
-// Criando função para preencher linhas do quadro de pixels
 const boardRow = (size) => {
-  // Criando linha
   const row = document.createElement('section');
   row.className = 'row';
   pixelBoard.appendChild(row);
-  // Preenchendo linha
   for (let index = 0; index < size; index += 1) {
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
@@ -69,11 +59,9 @@ const boardRow = (size) => {
     pixelNumber += 1;
   }
 };
-// Criando colunas
 // Requisito 15
 if (localStorage.getItem('boardSize') !== null) {
   const boardSize = localStorage.getItem('boardSize');
-  console.log(boardSize.length);
   for (let index = 0; index < boardSize; index += 1) {
     boardRow(boardSize);
   }
@@ -83,20 +71,17 @@ if (localStorage.getItem('boardSize') !== null) {
     boardRow(5);
   }
 }
-// Capturando class pixel elements
+// Requisito 9
 const pixelList = document.getElementsByClassName('pixel');
-// Criando função para selecionar elemento da paleta de cores
 for (let index = 0; index < colorBoxes.length; index += 1) {
   colorBoxes[index].addEventListener('click', (event) => {
-    // Removendo classe selected anterior
     const selected = document.querySelector('.selected');
     selected.classList.remove('selected');
-    // Adicionando classe selected ao elemento clicado
     event.target.classList.add('selected');
   });
 }
 
-// Criando objeto para estocar cores no localStorage, ou repopulando se já houverem pixels estocados.
+// Requisito 12
 const createPixelColorsObj = () => {
   if (localStorage.getItem('pixelBoard') === null) {
     return {};
@@ -105,13 +90,11 @@ const createPixelColorsObj = () => {
 };
 
 let pixelColorsStock = createPixelColorsObj();
-// Função para estocar cor do pixel no localStorage
 const pixelColorsToLocalStorage = (pixel) => {
   pixelColorsStock[pixel.id] = pixel.style.backgroundColor;
   localStorage.setItem('pixelBoard', JSON.stringify(pixelColorsStock));
 };
-//  Criando função para aplicar cor selecionada ao elemento do quadro de pixels
-// Event listener em todos os pixels
+// Requisito 10
 const ChangePixelCollor = () => {
   for (let index = 0; index < pixelList.length; index += 1) {
     pixelList[index].addEventListener('click', (event) => {
@@ -126,21 +109,19 @@ const ChangePixelCollor = () => {
   }
 };
 ChangePixelCollor();
-// Recuperando obj da localStorage
-const pixelColorsFromStorage = JSON.parse(localStorage.getItem('pixelBoard'));
-//  Função para resetar as cores dos pixels no storage
+// Requisito 11
 const resetStoragePixelColors = () => {
   for (let index = 0; index < pixelList.length; index += 1) {
     pixelList[index].style.backgroundColor = 'white';
-    // Resetando localStorage e objeto pixelColorsStock
     localStorage.removeItem('pixelBoard');
     pixelColorsStock = {};
   }
 };
-// Event listener para o botão reset resetar as cores
+const resetButton = document.getElementById('clear-board');
 resetButton.addEventListener('click', resetStoragePixelColors);
 
-//  Atribuindo cores, guardadas no localStorage, aos pixels
+// Requisito 12
+const pixelColorsFromStorage = JSON.parse(localStorage.getItem('pixelBoard'));
 const localStoragePixelsColors = () => {
   for (let index = 0; index < pixelList.length; index += 1) {
     const pixelName = pixelList[index].id;
@@ -150,14 +131,13 @@ const localStoragePixelsColors = () => {
 if (pixelColorsFromStorage !== null) {
   localStoragePixelsColors();
 }
-// Fazendo com que o valor do input seja transformado no tamanho dos pixels ao clicar no botão VQV
+// Requisito 13
 const rows = document.getElementsByClassName('row');
 const deleteOldBoard = () => {
   for (let index = rows.length - 1; index >= 0; index -= 1) {
     rows[index].remove();
   }
 };
-// Capturando elementos que serão usados
 const sizeInput = document.getElementById('board-size');
 const sizeButton = document.getElementById('generate-board');
 
@@ -166,9 +146,9 @@ sizeButton.addEventListener('click', () => {
     return alert('Board inválido!');
   }
   let inputNumber = +sizeInput.value;
-  if (inputNumber < 5) {
+  if (inputNumber < 5) { // Requisito 14
     inputNumber = 5;
-  } else if (inputNumber > 50) {
+  } else if (inputNumber > 50) { // Requisito 14
     inputNumber = 50;
   }
   pixelNumber = 0;
